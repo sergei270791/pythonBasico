f9149fce8001ae1a9c4c7d2d7d5c1726c170a475
sergei270791 <sergei.calle.c@uni.pe>
2021-10-29 03:05:17 -0500

    Primer programa

---
+++ conversor.py
@@ -0,0 +1,5 @@
+soles= float(input('Inserte la cantidad de soles a convertir: '))
+valor_dolar=3.95
+dolares= soles/valor_dolar
+dolares=round(dolares,2) # sirve para redondear a las cifras necesarias
+print('Tienes ',dolares,' dolares')
