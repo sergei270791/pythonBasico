362b39274eee0b1e03cd0fa80bb561d1dc11534c
sergei270791 <sergei.calle.c@uni.pe>
2021-10-30 17:22:55 -0500

    funciones

---
+++ funciones.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# def imprimir_mensaje():
+#     print('Mensaje especial: ')
+#     print('¡Estoy aprendiendo a usar funciones!')
+# imprimir_mensaje()
+# imprimir_mensaje()
+# imprimir_mensaje()
+
+
+# def conversacion(opcion):
+#     print('Hola😊')
+#     print('Cómo estás')
+#     print('Eligiste la opcion',opcion)
+#     print('Adios')
+
+
+# opcion = int(input('Elige una opción (1, 2, 3): '))
+# conversacion(opcion)
+
+def suma(a, b):
+    print('Se suman dos números')
+    resultado = a + b
+    return resultado
+
+sumatoria = suma(1, 4)
+print(sumatoria)
